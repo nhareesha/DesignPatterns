25912a6262cbf34e8bde442ccf2f5fdec72b94b5
hareesha <hareesha1906@gmail.com>
2016-09-01 14:53:30 -0700

    Add DTO

---
+++ designPatterns/src/main/java/com/java/designPatterns/FactoryPattern/Person.java
@@ -0,0 +1,23 @@
+package com.java.designPatterns.FactoryPattern;
+
+/**
+ * @author Hareesha
+ *
+ */
+public class Person {
+	private String name;
+	private String phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
