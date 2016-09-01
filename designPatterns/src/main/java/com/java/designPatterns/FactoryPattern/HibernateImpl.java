d87c6b929162ed1fab8774d974215b887af96833
hareesha <hareesha1906@gmail.com>
2016-09-01 14:54:41 -0700

    Add implementation classes for BaseInterface

---
+++ designPatterns/src/main/java/com/java/designPatterns/FactoryPattern/HibernateImpl.java
@@ -0,0 +1,13 @@
+package com.java.designPatterns.FactoryPattern;
+
+public class HibernateImpl implements BaseInterface {
+	
+	/**
+	 * Uses Hibernate connection 
+	 */
+	public int createRecord(Person person, String connType) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
---
+++ designPatterns/src/main/java/com/java/designPatterns/FactoryPattern/JDBCImpl.java
@@ -0,0 +1,13 @@
+package com.java.designPatterns.FactoryPattern;
+
+public class JDBCImpl implements BaseInterface {
+	
+	/**
+	 * Uses JDBC connector to connect to DB
+	 */
+	public int createRecord(Person person, String connType) {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
